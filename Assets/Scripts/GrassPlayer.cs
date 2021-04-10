74743421546af8583a81719648f2a02e59dedcf4
Efe Canoğlu <efe.canoglu@hotmail.com>
2021-04-10 16:56:42 +0300

    grass

---
+++ Assets/Scripts/GrassPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassPlayer : Player
+{   
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ChangePlayerControl();
+        }
+         if (Input.GetKeyDown(KeyCode.F))
+        {
+            TurnBackToOriginal();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(MoveHorizontal * speedMult * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpMult, ForceMode2D.Impulse);
+    }
+}
