4f9ae68787059a2516ce84c5cbb6a68765317367
mgbulat <m.gokhanbulat@gmail.com>
2021-04-10 12:13:59 +0300

    adana spor

---
+++ Assets/leafPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leafPlayer : Player
+{
+
+    public bool canMove;
+
+    public bool canJump;
+    [Range(0,1)]
+    public float WindSpeed;
+
+    private void Update()
+    {
+
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveVertical();
+        MoveHorizontal();
+        Wind();
+    }
+
+    private void Wind()
+    {
+       
+    }
+
+    private void MoveVertical()
+    {
+        float MoveVertical = Input.GetAxis("Vertical");
+        if (MoveVertical<0)
+        {
+            MoveVertical = 0;
+        }
+        Debug.Log(speedMult);
+
+        rb.velocity = new Vector2( rb.velocity.x,MoveVertical * speedMult * Time.deltaTime);
+    }
+
+    private void MoveHorizontal()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+
+        Debug.Log(speedMult);
+
+        rb.velocity = new Vector2(MoveHorizontal * speedMult * Time.deltaTime, rb.velocity.y);
+    }
+
+}
