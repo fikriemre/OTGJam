eb91d6c931708a39535b84d26cb9423c6f49c859
hostlean <hostlean@users.noreply.github.com>
2021-04-11 13:00:20 +0300

    Ground Check

--- Assets/Scripts/CubePlayer.cs
+++ Assets/Scripts/CubePlayer.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class CubePlayer : Player
 {
@@ -9,15 +11,22 @@ public class CubePlayer : Player
     public bool canMove;
 
     public bool canJump;
+
+
+    public float groundCheckDistance;
+    public LayerMask groundLayer;
+    
+    
     
     
     private void Update()
     {
-       
+        GroundCheck();
 
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if(GroundCheck())
+                Jump();
         }
         
         if(Input.GetKeyDown(KeyCode.F))
@@ -38,7 +47,7 @@ public class CubePlayer : Player
     {
         float moveHorizontal = Input.GetAxis("Horizontal");      
         rb.velocity = new Vector2(moveHorizontal * speedMult * Time.deltaTime, rb.velocity.y);
-        SFXManager.Instance.WalkSound( Mathf.Abs(moveHorizontal) >= 0.01f);
+        SFXManager.Instance.WalkSound( Mathf.Abs(moveHorizontal) >= 0.01f && GroundCheck());
         
     }
 
@@ -46,6 +55,16 @@ public class CubePlayer : Player
     {
         rb.AddForce(Vector2.up * jumpMult, ForceMode2D.Impulse);
         SFXManager.Instance.JumpSound();
+    }
+
+    public bool GroundCheck()
+    {
+        Vector2 rayOrigin = new Vector2(transform.position.x, collider.bounds.min.y);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, groundCheckDistance, groundLayer);
+        
+        Debug.DrawRay(rayOrigin, Vector2.down * groundCheckDistance, Color.green);
+
+        return hit.collider != null;
     }
     
 }
